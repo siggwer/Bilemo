c20535e18253b32a76cf108cef9e34c5bc6cf416
admin <admin@ETUDIANT.local>
2020-01-07 16:36:47 +0100

    create entity, database and fixtures

---
+++ src/Entity/Client.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+use Serializable;
+use Exception;
+
+/**
+ * Class Client
+ *
+ * @package App\Entity
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\ClientRepository")
+ */
+class Client
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank(message="This value should not be blank")
+     */
+    private $username;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank(message="This value should not be blank")
+     * @Assert\Email(message="Email address not valid")
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="This value should not be blank")
+     */
+    private $password;
+
+    /**
+     * @var string|null
+     *
+     * @Assert\NotBlank
+     *
+     * @Assert\Length(min=6)
+     */
+    private $plainPassword;
+
+    /**
+     * @var DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $created_at;
+
+    /**
+     * @var DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $updated_at;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="array")
+     */
+    private $roles = [];
+
+    /**
+     * @var
+     *
+     * @ORM\OneToMany(targetEntity="App\Entity\Consumer", mappedBy="client", orphanRemoval=true)
+     */
+    private $consumer;
+
+    /**
+     * Client constructor.
+     *
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->created_at = new DateTimeImmutable();
+        $this->updated_at = new DateTimeImmutable();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string|null $username
+     */
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string|null $plainPassword
+     */
+    public function setPlainPassword(?string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @return DateTimeImmutable|null
+     */
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param DateTimeImmutable|null $created_at
+     */
+    public function setCreatedAt(?DateTimeImmutable $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return DateTimeImmutable|null
+     */
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param DateTimeImmutable|null $updated_at
+     */
+    public function setUpdatedAt(?DateTimeImmutable $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRoles(): ?string
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param string|null $roles
+     */
+    public function setRoles(?string $roles): void
+    {
+        $this->roles = $roles;
+    }
+}
---
+++ src/Entity/Consumer.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+use Exception;
+
+/**
+ * Class Consumer
+ *
+ * @package App\Entity
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\ConsumerRepository")
+ */
+class Consumer
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank(message="This value should not be blank")
+     * @Assert\Email(message="Email address not valid")
+     */
+    private $email;
+
+    /**
+     * @var DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @var DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    /**
+     * @var Client
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\Client", inversedBy="Consumer")
+     * @ORM\JoinColumn(name="client_id", referencedColumnName="id")
+     */
+    public $client;
+
+    /**
+     * Consumer constructor.
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->created_at = new DateTimeImmutable();
+        $this->updated_at = new DateTimeImmutable();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return DateTimeImmutable|null
+     */
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param DateTimeImmutable|null $created_at
+     */
+    public function setCreatedAt(?DateTimeImmutable $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return DateTimeImmutable|null
+     */
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param DateTimeImmutable|null $updated_at
+     */
+    public function setUpdatedAt(?DateTimeImmutable $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
+
+}
