f90e9ea6686f281b49f88cd223ee29faa3ded1e9
admin <admin@ETUDIANT.local>
2020-02-07 15:28:01 +0100

    fix fixtures bug and read function

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use JMS\Serializer\SerializerInterface;
 use App\Repository\UserRepository;
 use App\Entity\User;
+
 
 /**
  * Class UserController
@@ -61,5 +63,82 @@ class UserController extends AbstractController
                 'json'
             ),
             Response::HTTP_OK, []);
+    }
+
+    /**
+     * @Route(name="user_create", methods={"POST"})
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param ValidatorInterface $validator
+     * @return Response
+     */
+    public function create(
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): Response
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, "json");
+
+        $constraintViolation = $validator->validate($user);
+
+        if($constraintViolation->count() > 0) {
+            return $this->json($constraintViolation, Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/{id}", name="user_update", methods={"PUT"})
+     *
+     * @param User $user
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param ValidatorInterface $validator
+     * @return Response
+     */
+    public function update(
+        User $user,
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): Response
+    {
+        $user = $serializer->deserialize(
+            $request->getContent(),
+            User::class,
+            "json",
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $user]
+        );
+
+        $constraintViolation = $validator->validate($user);
+
+        if($constraintViolation->count() > 0) {
+            return $this->json($constraintViolation, Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Route("/{id}", name="user_delete", methods={"DELETE"})
+     *
+     * @param User $user
+     * @return Response
+     */
+    public function delete(User $user): Response
+    {
+        $this->getDoctrine()->getManager()->remove($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
