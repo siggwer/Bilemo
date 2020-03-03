d092e92d981453b670665fa93f7296a5b99f3d6b
admin <admin@ETUDIANT.local>
2020-03-03 16:43:23 +0100

    add authentificated client in product controller test

--- tests/FunctionalTests/ProductControllerTest.php
+++ tests/FunctionalTests/ProductControllerTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\FunctionalTests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
 
 /**
  * Class ProductControllerTest
@@ -12,24 +14,46 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class ProductControllerTest extends WebTestCase
 {
     /**
+     * @param string $username
+     * @param string $password
+     *
+     * @return KernelBrowser
+     */
+    protected function createAuthenticatedClient($username = 'email10@email.fr', $password = 'password')
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/login_check',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            json_encode(array(
+                '_username' => $username,
+                '_password' => $password,
+            ))
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        dd(json_decode($client));
+
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+        return $client;
+    }
+    
+    
+    /**
      *
      */
     public function testApiProductControllerResponseOk()
     {
-        $client = static::createClient();
+        $client = $this->createAuthenticatedClient();
 
         $client->request('GET', '/api/products');
 
-        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
-
-//    public function testApiProductControllerResponseUnauthorized()
-//    {
-//        $client = static::createClient();
-//
-//        $client->request('GET', '/api/products');
-//
-//        $this->assertEquals(401, $client->getResponse()->getStatusCode());
-//    }
 
 }
